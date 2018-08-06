1d57a84f91af061bc6f1b1bdb3571c4a414ae39d
Martti <martti@aukia.com>
2018-08-06 22:40:11 +0300

    removed unnecessary test file

--- index.test.ts
+++
@@ -1,6 +0,0 @@
-import { convertUrlToBoard, saveArrayToPaprika, getPins, getLinks } from './index'
-
-test("convert url to board name", () => {
-    expect(convertUrlToBoard('https://fi.pinterest.com/aukia/recipes-for-bbq/')).toBe("boards/aukia/recipes-for-bbq/pins")
-});
-
